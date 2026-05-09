1a74d67cf2e4a3b1cc61ba0b7ec850b50ed26d99
Nane <kds.napha@gmail.com>
2026-05-10 04:24:23 +0900

    add RegisterCustomerCommand interface

--- src/modules/customer/application/use-cases/commands/register-customer.command.ts
+++ src/modules/customer/application/use-cases/commands/register-customer.command.ts
@@ -1,3 +1,9 @@
+export interface RegisterCustomerCommand {
+  readonly name: string;
+  readonly email: string;
+  readonly phone: string;
+  readonly password: string;
+}
 export interface RegisterCustomerResult {
   readonly id: string;
   readonly name: string;
