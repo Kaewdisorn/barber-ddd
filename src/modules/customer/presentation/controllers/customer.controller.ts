ef9149008a8d154a55144852a2245224607ec877
Nane <kds.napha@gmail.com>
2026-05-10 04:28:58 +0900

    controller call registerCustomer command

--- src/modules/customer/presentation/controllers/customer.controller.ts
+++ src/modules/customer/presentation/controllers/customer.controller.ts
@@ -1,16 +1,22 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { RegisterCustomerResult } from '@customerModule/application/use-cases/commands/register-customer.command';
 import { RegisterCustomerRequestDto } from '@customerModule/presentation/dtos/register-customer-request.dto';
+import { RegisterCustomerHandler } from '@customerModule/application/use-cases/handlers/register-customer.handler';
 
 @Controller('customers')
 export class CustomerController {
-  constructor() {}
+  constructor(private readonly registerCustomer: RegisterCustomerHandler) {}
 
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
   async register(
     @Body() dto: RegisterCustomerRequestDto,
   ): Promise<RegisterCustomerResult> {
-    return {} as RegisterCustomerResult;
+    return this.registerCustomer.execute({
+      name: dto.name,
+      email: dto.email,
+      phone: dto.phone,
+      password: dto.password,
+    });
   }
 }
