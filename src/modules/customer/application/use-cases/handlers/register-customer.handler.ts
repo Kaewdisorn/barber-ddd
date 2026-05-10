8a0d531adda173dacfded6f8fc464eda74bca94e
Nane <kds.napha@gmail.com>
2026-05-11 06:16:19 +0900

    inject customerRepository

--- src/modules/customer/application/use-cases/handlers/register-customer.handler.ts
+++ src/modules/customer/application/use-cases/handlers/register-customer.handler.ts
@@ -1,14 +1,27 @@
+import { Inject, Injectable } from '@nestjs/common';
 import {
   RegisterCustomerCommand,
   RegisterCustomerResult,
 } from '@customerModule/application/use-cases/commands/register-customer.command';
+import {
+  CUSTOMER_REPOSITORY,
+  ICustomerRepository,
+} from '@customerModule/application/ports/customer-repository.port';
 
+@Injectable()
 export class RegisterCustomerHandler {
-  constructor() {}
+  constructor(
+    @Inject(CUSTOMER_REPOSITORY)
+    private readonly customerRepository: ICustomerRepository,
+  ) {}
 
   async execute(
     command: RegisterCustomerCommand,
   ): Promise<RegisterCustomerResult> {
+    const emailTaken = await this.customerRepository.existsByEmail(
+      command.email,
+    );
+
     console.log('Registering customer with data:', command);
     return {} as RegisterCustomerResult;
   }
