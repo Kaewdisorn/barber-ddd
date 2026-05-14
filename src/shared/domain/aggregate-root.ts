94ab77ec9e3b91c6972b4669098768567f71a201
Nane <kds.napha@gmail.com>
2026-05-15 02:03:10 +0900

    create AggregateRoot

---
+++ src/shared/domain/aggregate-root.ts
@@ -0,0 +1,15 @@
+import { DomainEvent } from '@shared/domain/domain-event';
+
+export abstract class AggregateRoot {
+  private _domainEvents: DomainEvent[] = [];
+
+  protected addDomainEvent(event: DomainEvent): void {
+    this._domainEvents.push(event);
+  }
+
+  public pullDomainEvents(): DomainEvent[] {
+    const events = [...this._domainEvents];
+    this._domainEvents = [];
+    return events;
+  }
+}
