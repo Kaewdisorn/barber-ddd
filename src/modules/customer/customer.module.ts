4ba13a261986b1cbec1d68913626a056211fbc43
Nane <kds.napha@gmail.com>
2026-05-19 08:10:36 +0900

    add CUSTOMER_REPOSITORY provider

--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CustomerController } from '@customerModule/presentation/controllers/customer.controller';
 import { RegisterCustomerHandler } from '@customerModule/application/use-cases/handlers/register-customer.handler';
+import { PrismaCustomerRepository } from '@customerModule/infrastructure/persistence/prisma-customer.repository';
+import { CUSTOMER_REPOSITORY } from '@customerModule/application/ports/customer-repository.port';
 
 @Module({
   imports: [],
   controllers: [CustomerController],
-  providers: [RegisterCustomerHandler],
+  providers: [
+    RegisterCustomerHandler,
+    { provide: CUSTOMER_REPOSITORY, useClass: PrismaCustomerRepository },
+  ],
 })
 export class CustomerModule {}
