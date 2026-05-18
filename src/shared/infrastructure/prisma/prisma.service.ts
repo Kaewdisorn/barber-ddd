d2cf2ba192dfb250405ea2b5d32a13aae5cd7a92
Nane <kds.napha@gmail.com>
2026-05-18 12:32:22 +0900

    update prisma 7 error

---
+++ src/shared/infrastructure/prisma/prisma.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+const createPrismaClient = () => {
+  const pool = new Pool({ connectionString: process.env['DATABASE_URL'] });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
+};
+
+type PrismaClientInstance = ReturnType<typeof createPrismaClient>;
+
+@Injectable()
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  private readonly client: PrismaClientInstance;
+
+  constructor() {
+    this.client = createPrismaClient();
+  }
+
+  get customer() {
+    return this.client.customer;
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.client.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.client.$disconnect();
+  }
+}
