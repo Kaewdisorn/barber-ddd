6785d5b2bc5218c56617bfd3e37fd1c1a33e3be2
Nane <kds.napha@gmail.com>
2026-05-18 13:08:02 +0900

    add PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { CustomerModule } from '@customerModule/customer.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from '@shared/infrastructure/prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), CustomerModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    CustomerModule,
+  ],
   controllers: [],
   providers: [],
 })
