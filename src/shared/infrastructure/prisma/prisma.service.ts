cb6d26de567a0be4747bd02b6e68610b36fca725
Nane <kds.napha@gmail.com>
2026-05-18 13:35:07 +0900

    add DB connectionTimeout

--- src/shared/infrastructure/prisma/prisma.service.ts
+++ src/shared/infrastructure/prisma/prisma.service.ts
@@ -4,7 +4,11 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
 const createPrismaClient = () => {
-  const pool = new Pool({ connectionString: process.env['DATABASE_URL'] });
+  const pool = new Pool({
+    connectionString: process.env['DATABASE_URL'],
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 5000,
+  });
   const adapter = new PrismaPg(pool);
   return new PrismaClient({ adapter });
 };
